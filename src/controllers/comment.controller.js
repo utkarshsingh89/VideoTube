459e61740dfac1a150cf25ece3a74b90adca444b
Utkarsh kumar singh <utkarsh.kumar.singh8932@gmail.com>
2026-04-10 00:37:56 +0530

    Enhance comment functionality: implement update and delete operations with validation and authorization checks; add pagination and user details fetching for comments.

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -29,12 +29,53 @@ const addComment=asyncHandler(async(req,res)=>{
 })
 //update comment
 const updateComment=asyncHandler(async(req,res)=>{
+    const {content}=req.body;
+    const {CommentId}=req.params;
+    if(!content){
+        throw new apierror(400,"comment is missing");
+    }
+    if(!isValidObjectId(CommentId)){
+        throw new apierror(400,"comment is missing");
+    }
+    const comment=await Comment.findById(CommentId);
+    if(!comment){
+        throw new apierror(404,"comment is invalid");
+    }
+    if(comment.owner.toString()!==req.user._id.toString()){
+        throw new apierror(403,"you are not authorized");
+    }
+    comment.content=content ||comment.content;
+    await comment.save();
+
+    return res.status(200).json(new apiresponse(200,comment,"update comment successfully"))
 
 })
 
 
 //delete comment
 const deleteComment=asyncHandler(async(req,res)=>{
+    const {commentId}=req.params;
+    if(!isValidObjectId(CommentId)){
+        throw new apierror(400,"comment is missing");
+    }
+
+    const comment=await Comment.findById(CommentId);
+    if(!comment){
+        throw new apierror(404,"comment is invalid");
+    }
+    if(comment.owner.toString()!==req.user._id.toString()){
+        throw new apierror(403,"you are not authorized");
+    }
+     const deletedComment = await Comment.findOneAndDelete({
+        _id: commentId,
+        owner: req.user._id   // 🔐 auth check
+    });
+
+    // 3. If not found → either not exist OR not owner
+    if (!deletedComment) {
+        throw new apiError(404, "Comment not found or unauthorized");
+    }
+    return res.status(200).json(new apiresponse(200,deleteComment,"comment deleted"));
     
 })
 
@@ -45,5 +86,59 @@ const deleteComment=asyncHandler(async(req,res)=>{
 const getComment=asyncHandler(async(req,res)=>{
     const { videoId } = req.params;
     const { page = 1, limit = 10 } = req.query;
+     const Page=parseInt(page);
+    const Limit=parseInt(limit);
+    if(!isValidObjectId(videoId)){
+        throw new apierror(400,"videoid is missing");
+    }
+    const userId=req.user._id;
+    if(!userId||!isValidObjectId(userId)){
+        throw new apierror(400,"userid is not valid")
+    }
+    const getallComment=await Comment.aggregate([
+        {
+            $match:{
+                video:new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $sort:{createdAt:-1}
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"commentor_details"
+            }
+        },
+        {
+            $unwind:"$commentor_details"
+        },
+        {
+            $project:{
+
+                content:1,
+                createdAtL1,
+                "commentor_details.username":1,
+                "commentor_details.avtar":1,
+                "commentor_details.Fullname":1,
+                "commentor_details.createdAt":1,
+
+            }
+        }
+    ])
+    if(!getallComment){
+        throw new apierror(404,"comment not found");
+    }
+    const fetchedComment=Comment.aggregatePaginate(getallComment,{
+        page:Page,
+        limit:Limit
+    })
+    return res.status(200).json(
+        new apiresponse(200,fetchedComment,"comment fetched successfully")
+    );
+
+
 })
 
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,144 @@
+import mongoose from "mongoose"
+import {Video} from "../models/video.model.js"
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import { apierror } from "../utils/apierror.js";
+import { apiresponse } from "../utils/apiresponse.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { User } from "../models/user.model.js";
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const getdetails=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscription_details"
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"_id",
+                foreignField:"owner",
+                as:"video_details"
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+
+    // Step 1: take your video IDs
+                 let: { videoIds: "$video_details._id" },
+
+                pipeline: [
+                {
+                $match: {
+                    $expr: {
+            // Step 2: filter likes
+                    $in: ["$video", "$$videoIds"]
+                     }
+                }
+            }
+            ],
+
+            as: "likes"
+             }
+        },
+         {
+            $addFields: {
+                // ✅ Total Videos
+                totalVideos: { $size: "$video_details" },
+
+                // ✅ Total Subscribers
+                totalSubscribers: { $size: "$subscription_details" },
+
+                // ✅ Total Likes
+                totalLikes: { $size: "$likes" },
+
+                // ✅ Total Views (handle array or number)
+                totalViews: {
+                    $sum: {
+                        $map: {
+                            input: "$video_details",
+                            as: "video",
+                            in: {
+                                $cond: {
+                                    if: { $isArray: "$$video.views" },
+                                    then: { $size: "$$video.views" },
+                                    else: { $ifNull: ["$$video.views", 0] }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        {
+            $project: {
+                username: 1,
+                totalVideos: 1,
+                totalSubscribers: 1,
+                totalLikes: 1,
+                totalViews: 1
+            }
+        }
+
+    ])
+    return res.status(200).json(new apiresponse(299,getdetails,"details fetched successfully"))
+
+
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+    if(!req.user._id){
+        throw new apierror(400,"user is missing");
+    }
+    const getvideo=await Video.aggregate([
+      {
+        $match:{
+            owner:new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+      {
+        $sort:{
+            createdAt:-1
+        }
+      },
+      {
+        $addFields:{
+            views:{
+                $cond:{
+                    if:{$isArray:"$views"},
+                    then:{$size:"$views"},
+                    else:{$ifnull:["$views",0]}
+                }
+            }
+        }
+      },
+      {
+        $project:{
+            thumbnail:1,
+            views:1,
+            createdAt:1,
+            title:1
+        }
+      }
+    ])
+    return res.status(200).json(
+        new apiresponse(200,getvideo,"video fetched successfully")
+    );
+})
+
+export {
+    getChannelStats, 
+    getChannelVideos
+    }
