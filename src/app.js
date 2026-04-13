4aba1aeacf6033777126451d8fdcfe82556bca20
Utkarsh kumar singh <utkarsh.kumar.singh8932@gmail.com>
2026-04-13 23:52:47 +0530

    correct router path

--- src/app.js
+++ src/app.js
@@ -24,13 +24,13 @@ import userRouter from "./route/user.routes.js";
 app.use("/users",userRouter);
 
 
-import videoRouter from "./route/video.route.js";
+import videoRouter from "./route/video.routes.js";
 app.use("/videos",videoRouter);
 
 import playlistRouter from "./route/playlist.routes.js";
 app.use("/playlists",playlistRouter);
 
-import dashboardRouter from "./route/dashboard.rotes.js";
+import dashboardRouter from "./route/dashboard.routes.js";
 app.use("/dashboard",dashboardRouter);
 
 import commentRouter from "./route/comment.routes.js";
@@ -45,7 +45,7 @@ app.use("/tweets",tweetRouter);
 import likeRouter from "./route/like.routes.js";
 app.use("/likes",likeRouter);
 
-import healthCheckRouter from "./route/healthcheck.rotes.js";
+import healthCheckRouter from "./route/healthcheck.routes.js";
 app.use("/healthcheck",healthCheckRouter);
 
 
